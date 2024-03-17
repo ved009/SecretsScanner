c3138df38302cd14b45973f519d2601db81a4a76
Vedant Roy <vedantroy42@gmail.com>
2024-03-17 10:57:51 +0530

    Update secrets.sh

--- secrets.sh
+++ secrets.sh
@@ -5,7 +5,7 @@ echo -e "\n-----------------------COLLECTING URLS------------------------"
 cat target.txt | gau | sort -u >> allfile_links.txt;
 cat target.txt | httpx -mc 200 -silent | katana -jc -aff -d 5 -jsl -kf all -iqp -silent | sort -u >> allfile_links.txt;
 cat allfile_links.txt | grep -iE "\.js$" | sort -u >> jsfile_links.txt;
-cat jsfile_links.txt | sort -u | httpx -mc 200 -follow-redirects -silent > live_jsfile_links.txt;
+cat jsfile_links.txt | sort -u | httpx -mc 200 -silent > live_jsfile_links.txt;
 
 echo -e "\n-----------------------COLLECTING JS FILES LOCALLY------------------------"
 mkdir -p jsfiles;
